20286186e406ac83cf40d2352e15628773cb851d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-09 08:52:41 +0100

    Constant values out of range

---
+++ 06_integer_signed_types_checks.go
@@ -0,0 +1,24 @@
+// Seriál "Programovací jazyk Go"
+//
+// Druhá část:
+//    Datové typy v programovacím jazyku Go
+//    https://www.root.cz/clanky/datove-typy-v-programovacim-jazyku-go/
+//
+// Demonstrační příklad číslo 2:
+//    Kontrola celočíselných konstant
+
+package main
+
+import "fmt"
+
+func main() {
+	var a int8 = -1000
+	var b int16 = -100000
+	var c int32 = -10000000000
+	var d int32 = -10000000000000000
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
