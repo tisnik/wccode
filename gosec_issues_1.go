d27d69d6f3b38accf034689219701e288b113803
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-14 08:13:47 +0100

    Example for gosec issues #1

---
+++ gosec_issues_1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"os"
+)
+
+// PipelineLogEntry represents one log entry (record) read from log file.
+type PipelineLogEntry struct {
+	Level    string `json:"levelname"`
+	Time     string `json:"asctime"`
+	Name     string `json:"name"`
+	Filename string `json:"filename"`
+	Message  string `json:"message"`
+}
+
+func readPipelineLogFile(filename string) ([]PipelineLogEntry, error) {
+	entries := []PipelineLogEntry{}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return entries, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		entry := PipelineLogEntry{}
+		err = json.Unmarshal([]byte(scanner.Text()), &entry)
+		if err != nil {
+			log.Println(err)
+		} else {
+			entries = append(entries, entry)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return entries, err
+	}
+
+	return entries, nil
+}
+
+func main() {
+	readPipelineLogFile("foobar")
+}
